70e9fb54d7fd0dac3e6ec17eac149e07c1eb74c1
timotheepoisot (Mr ) <tpoisot@um2.fr>
2009-12-12 18:14:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/scian@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SCiAn/R/fromlayout.r
+++ pkg/SCiAn/R/fromlayout.r
@@ -1,13 +1,12 @@
-fromlayout <- function(layout,data)
+fromlayout = function (layout, data) 
 {
-	results <- list()
-	samplenames <- unique(layout[,1])
-	for(i in 1:length(samplenames))
-	{
-		pos.todo <- subset(layout[,2],layout[,1]==samplenames[i])
-		partial.results <- as.vector(data[pos.todo])
-		results[[length(results)+1]] <- partial.results
-	}
-	names(results) <- samplenames
-	return(results)	
+    results <- list()
+    samplenames <- unique(layout[, 1])
+    for (i in 1:length(samplenames)) {
+        pos.todo <- subset(layout[, 2], layout[, 1] == samplenames[i])
+        partial.results <- as.vector(data[pos.todo])
+        results[[length(results) + 1]] <- as.numeric(partial.results)
+    }
+    names(results) <- samplenames
+    return(results)
 }
