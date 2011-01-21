8425b7094623a75bac4df0a430c7c70f8db6a313
timotheepoisot (Mr ) <tpoisot@um2.fr>
2011-01-21 22:02:38 +0000

    Maintenance
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/scian@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SCiAn/R/fromlayout.r
+++
@@ -1,9 +0,0 @@
-fromlayout = function (layout, data) 
-{
-
-	data<-layout[,1][match(names(data),layout[,2])]
-	OK<-!is.na(Nam)
-	out<-as.data.frame(cbind(Cond=Nam[OK],Val=data[OK]))
-    results <- lapply(split(out$Val,out$Cond),function(x)as.numeric(as.vector(x)))
-    return(results)
-}
